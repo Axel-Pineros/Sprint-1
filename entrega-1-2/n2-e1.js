c9cf78dd08204c4611fd2b453f3c46f9965afd66
Axel Piñeros <axelpineros@outlook.com>
2023-03-29 09:25:47 +0200

    Fet n2-e1

--- entrega-1-2/n2-e1.js
+++ entrega-1-2/n2-e1.js
@@ -1,5 +1,13 @@
 // Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
 
+const completeName = {
+    firstName: "Pedro",
+    lastName: "Picapiedra",
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
 ((e) => {
     console.log((`Hola, em dic ${e}`));
-})("Ramon");
+})(completeName.fullName());
