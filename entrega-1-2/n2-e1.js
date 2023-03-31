4efc052f8c814f2db53c0fc4855e7955fe256b93
Axel Piñeros <axelpineros@outlook.com>
2023-03-31 18:13:50 +0200

    Correcció

--- entrega-1-2/n2-e1.js
+++ entrega-1-2/n2-e1.js
@@ -1,13 +1,9 @@
 // Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
 
-const completeName = {
-    firstName: "Pedro",
-    lastName: "Picapiedra",
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
+((name) => {
+    const personName = {
+        "firstname": name,
     }
-};
+    return personName;
+})("Pedro");
 
-((e) => {
-    console.log((`Hola, em dic ${e}`));
-})(completeName.fullName());
