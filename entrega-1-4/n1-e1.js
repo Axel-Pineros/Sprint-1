c23c0fd0a6a43681f8dd098cc3c42adc68c812d8
Axel Piñeros <axelpineros@outlook.com>
2023-04-02 14:19:22 +0200

    Primera aproximació a l'exercici

--- entrega-1-4/n1-e1.js
+++ entrega-1-4/n1-e1.js
@@ -22,12 +22,47 @@ let salaries = [{
     salary: 2000
 }];
 
-async function functAsync(e) {
-    getEmployee(e);
-    getSalary(e);
+async function resultatFinal(e) {
+    getEmployee(e).
+        then((resultado) => {
+            console.log(resultado);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    getSalary(e)
+        .then((resultado) => {
+            console.log(resultado);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
-const getEmployee = e => console.log(employees[e - 1].name);
-const getSalary = e => console.log(salaries[e - 1].salary);
+// Canvia el valor d'aquesta variable per obtenir el nom de l'employee amb un id igual al número de la variable
+const idSearch = 3;
 
-functAsync(employees[0].id);
+const getEmployee = (e) => {
+    return new Promise((resolve, reject) => {
+        const employeeObject = employees.find(item => item.id === e);
+        if (employeeObject !== undefined) {
+            resolve(`El nom de l'employee és ${employeeObject.name}`);
+        } else {
+            reject("¡Aquest id no té associat cap nom!");
+        }
+    });
+}
+
+const getSalary = (e) => {
+    return new Promise((resolve, reject) => {
+        const employeeObject = employees.find(item => item.id === e);
+        if (employeeObject !== undefined) {
+            const objectSalaries = salaries.find(item => item.id === e);
+            resolve(`${employeeObject.name} té un salari de ${objectSalaries.salary}`);
+        } else {
+            reject("¡Aquest id no té associat cap salari!");
+        }
+    });
+}
+
+resultatFinal(idSearch);
