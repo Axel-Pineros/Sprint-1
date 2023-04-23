ed321bf775a3638108a69d4e325160ce417bf02c
Axel Piñeros <axelpineros@outlook.com>
2023-04-23 12:24:16 +0200

    Afegit línia buida

--- entrega-1-5/n1-e1.js
+++ entrega-1-5/n1-e1.js
@@ -1,6 +1,7 @@
 // Crea una funció que, en executar-la, escrigui una frase en un fitxer.
 
 const fs = require('fs');
+
 fs.appendFile('fitxer.txt', 'Hola que tal.', function (err) {
     if (err) throw err;
     console.log('Updated!');
