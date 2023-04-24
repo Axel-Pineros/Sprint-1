260aa8774b95961cb1d4caca553aa45b3e63c689
Axel Piñeros <axelpineros@outlook.com>
2023-04-24 11:39:21 +0200

    Simplificar i desplaçar errors

--- entrega-1-4/n2-e1.js
+++ entrega-1-4/n2-e1.js
@@ -1,34 +1,29 @@
 // Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons.
 
-async function doubleNumber(e) {
-    if (e === null || e === undefined || e === '') {
-        throw new Error(`No s'han introduit cap valor`);
-    }
-    if (typeof e !== "number") {
-        throw new Error(`Cal introduir un número`);
-    }
+function doubleNumber(e) {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve(e * 2);
-        }, 2000, e);
+        }, 2000);
     });
 }
-
-doubleNumber(8).then((resultado) => {
-    console.log(resultado);
-})
 
 //  Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior.
 
 async function suma3numeros(e, r, t) {
     if (e === null || e === undefined || e === '' || r === null || r === undefined || r === '' || t === null || t === undefined || t === '') {
+        throw new Error(`No s'han introduit tots els valors`);
+    }
+    if (typeof e !== "number" || typeof r !== "number" || typeof t !== "number") {
         throw new Error(`No s'han introduit 3 números`);
     }
+
     const num1 = await doubleNumber(e);
     const num2 = await doubleNumber(r);
     const num3 = await doubleNumber(t);
-    const resultado = num1 + num2 + num3;
-    console.log(resultado);
+    return num1 + num2 + num3;
 }
 
 suma3numeros(2, 4, 5);
+
+module.exports = { doubleNumber, suma3numeros };
